48b6e1be8eaa3e22c89059068ca1ecaead3409cf
JPSanin <juanpablosaninb@gmail.com>
2021-08-27 19:59:46 -0500

    Data table implemented

--- workshop-government-data/MainWindow.xaml.cs
+++ workshop-government-data/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,33 +21,64 @@ namespace workshop_government_data
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    
     public partial class MainWindow : Window
     {
+
+        
         public MainWindow()
         {
             InitializeComponent();
+           
+
         }
+
+        
 
         private void ButtonLoadFile(object sender, RoutedEventArgs e)
         {
+            var dataTable = new DataTable();
+
             OpenFileDialog open = new OpenFileDialog();
+
             open.Filter= "csv files (*.csv)|*.csv|All files (*.*)|*.*";
             var filePath = string.Empty;
 
             if (open.ShowDialog() == true)
             {
-                
+
                 loadBtn.Visibility = Visibility.Hidden;
 
                 var path = open.OpenFile();
                 using (StreamReader reader = new StreamReader(path))
                 {
-                    var line = string.Empty;
+                    var line = reader.ReadLine();
+                    String[] lineInfoTitles = line.Split(",");
+
+                    dataTable.Columns.Add(lineInfoTitles[0], typeof(String));
+                    dataTable.Columns.Add(lineInfoTitles[1], typeof(String));
+                    dataTable.Columns.Add(lineInfoTitles[2], typeof(String));
+                    dataTable.Columns.Add(lineInfoTitles[3], typeof(String));
+                    dataTable.Columns.Add(lineInfoTitles[4]);
+
+
                     while ((line = reader.ReadLine()) != null)
                     {
-                        MessageBox.Show(line);
+
+                        String[] lineInfo = line.Split(",");
+                        //MessageBox.Show(lineInfo[4]);
+                        if(lineInfo[0].Length<3)
+                        {
+                            dataTable.Rows.Add(lineInfo);
+                        }
+
                     }
                 }
+                
+                dataGrid.DataContext = dataTable.DefaultView;
+                
             }
 
         }
